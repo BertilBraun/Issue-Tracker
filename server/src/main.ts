e1994e4a660586afbb237467ac7655b28550369f
Bertil <bertil.braun.private@gmail.com>
2022-01-24 14:05:53 +0100

    add swagger docs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
   // middleware
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api')
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
+  })
+
+  const config = new DocumentBuilder()
+    .setTitle('Issue Tracker API')
+    .setDescription('Track issues for your projects, comments, and more.')
+    .setVersion('0.1')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
 
   // start server
-  await app.listen(3000);
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
