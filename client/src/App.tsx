ce5e9164b035a12b8f42117ef2cd642f420eae10
Bertil <bertil.braun.private@gmail.com>
2022-01-24 14:05:06 +0100

    fix client setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import PageTemplate from './components/home/PageTemplate'
+import Footer from './components/home/Footer'
+import NavBar from './components/home/NavBar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Project from './pages/Project'
@@ -10,14 +10,14 @@ import SignUp from './pages/SignUp'
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <PageTemplate>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/project/:projectId" element={<Project />} />
-        </Routes>
-      </PageTemplate>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/project/:projectId" element={<Project />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
